e59be9b0b527f3cb139d66c80847afc53958ce0c
Chudy4822 <jamarcin2901@wp.pl>
2020-07-23 18:35:45 +0200

    co teraz

---
+++ src/main/java/com/example/demofdswvf/DemofdswvfApplication.java
@@ -0,0 +1,13 @@
+package com.example.demofdswvf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemofdswvfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemofdswvfApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demofdswvf/DemofdswvfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demofdswvf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemofdswvfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
